6aa04f4f86a4103bc7c6684138c19527fa0b1c3d
James Hu <axsuul@gmail.com>
2012-02-25 13:54:39 -0800

    add focus support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,8 @@ require 'underglow' # and any other gems you need
 
 RSpec.configure do |config|
   # some (optional) config here
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
