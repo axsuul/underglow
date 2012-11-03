b88a554de4297d71f849d3bc1516c462f7f7ab90
James Hu <axsuul@gmail.com>
2012-11-02 22:53:12 -0700

    Move tasks to namespace directory

--- lib/underglow.rb
+++ lib/underglow.rb
@@ -9,7 +9,7 @@ module Underglow
       class Railtie < ::Rails::Railtie
         rake_tasks do
           # load all rake tasks, need to load them by full path so they don't get confused with any tasks of the same name in Rails app
-          Dir[File.join(File.dirname(__FILE__), 'tasks/*.rake')].each { |f| load f }
+          Dir[File.join(File.dirname(__FILE__), 'underglow/tasks/*.rake')].each { |f| load f }
         end
       end
     end
