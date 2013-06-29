60871af36fe8d5dfbfdab3a5b85b6bcf50021167
James Hu <hello@james.hu>
2013-06-29 12:43:54 -0700

    Fix rails recipes

--- lib/underglow/capistrano/deploy/rails.rb
+++ lib/underglow/capistrano/deploy/rails.rb
@@ -1,10 +1,5 @@
 Capistrano::Configuration.instance.load do
   namespace :rails do
-    desc "Estbalish SSH session into rails directory"
-    task :ssh, roles: :app do
-      run_remote "cd #{current_path}"
-    end
-
     desc "Run rails console"
     task :console, roles: :app do
       run_remote "cd #{current_path} && #{deploy_to}/environment bin/rails c"
@@ -16,7 +11,7 @@ Capistrano::Configuration.instance.load do
     end
 
     desc "Tail rails log"
-    task :log, roles: :app do
+    task :tail, roles: :app do
       run_remote "cd #{current_path} && tail -f log/#{stage}.log"
     end
   end
